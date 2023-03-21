6328ee66f4b3d245074bc397d6850258773a8b4a
Victor012396 <victor.velasco01@hotmail.com>
2023-03-21 00:17:44 -0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<x-layout bodyClass="">
+    <div class="container position-sticky z-index-sticky top-0">
+        <div class="row">
+            <div class="col-12">
+                <x-navbars.navs.guest signup='register' signin='login'></x-navbars.navs.guest>
+            </div>
+        </div>
+    </div>
+    <div class="page-header justify-content-center min-vh-100"
+        style="background-image: url('https://img.freepik.com/free-photo/cloud-computing-banner-background-smart-city_53876-108504.jpg?w=740&t=st=1679378996~exp=1679379596~hmac=8e0d8fd7bff9687b6cba4f90861e05d95132922b5cb5f14cd57b6ccb4dbf09b2');">
+        <span class="mask bg-gradient-dark opacity-6"></span>
+        <div class="container">
+            <h1 class="text-light text-center">Welcome to Material Dashboard FREE Laravel Live Preview.</h1>
+        </div>
+    </div>
+        <x-footers.guest></x-footers.guest>
+</x-layout>
